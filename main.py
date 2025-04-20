a2d05272446b22f301ddba4f22a8e774d5b26de9
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-04-20 17:15:05 +0200

    02: Parsing a two-dimensional array to a .qml view.

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+from __future__ import annotations
+
+from pathlib import Path
 import sys
+from PySide6.QtCore import QUrl
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQuick import QQuickView
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
-    engine = QQmlApplicationEngine()
-    engine.addImportPath(sys.path[0])
-    engine.loadFromModule("folder", "Main")
-    if not engine.rootObjects():
-        sys.exit(-1)
+        
+    dataList = [
+            ["name", "surname", "1", "City", "Street"],
+            ["name", "surname", "1", "City", "Street"]
+    ]
+
+    view = QQuickView()
+    view.setInitialProperties({"model":dataList})
+    view.setSource(QUrl.fromLocalFile(Path(__file__).parent/"folder"/"view.qml"))
+    view.show()
+
     exit_code = app.exec()
-    del engine
+    del view
     sys.exit(exit_code)
 
