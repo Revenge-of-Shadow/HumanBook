7b1c150aff6048f6e7a67e854ef9925d2f28f8bf
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-04-20 18:00:16 +0200

    03: To hell with Qt!

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-from __future__ import annotations
+from tkinter import *
 
-from pathlib import Path
-import sys
-from PySide6.QtCore import QUrl
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtQuick import QQuickView
+root = Tk()
+root.geometry('600x400')
+root.title("")
 
-if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
-        
-    dataList = [
-            ["name", "surname", "1", "City", "Street"],
-            ["name", "surname", "1", "City", "Street"]
-    ]
-
-    view = QQuickView()
-    view.setInitialProperties({"model":dataList})
-    view.setSource(QUrl.fromLocalFile(Path(__file__).parent/"folder"/"view.qml"))
-    view.show()
-
-    exit_code = app.exec()
-    del view
-    sys.exit(exit_code)
-
+a = Label(root, text = "Text.")
+a.pack()
+root.mainloop()
