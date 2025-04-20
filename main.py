586604db2f2c324c8d8cadfe4e72e593336da59f
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-04-20 19:34:14 +0200

    05: Listbox.

--- main.py
+++ main.py
@@ -6,4 +6,17 @@ root.title("")
 
 a = Label(root, text = "Text.")
 a.pack()
+
+scroll_bar = Scrollbar(root)
+scroll_bar.pack(side=RIGHT, fill = Y)
+
+mylist = Listbox(root, yscrollcommand=scroll_bar.set, width = 580, bg="grey")
+
+for line in range (1, 4):
+    mylist.insert(END, str(line))
+
+mylist.pack(side = LEFT, fill = BOTH)
+
+scroll_bar.config(command = mylist.yview)
+
 root.mainloop()
