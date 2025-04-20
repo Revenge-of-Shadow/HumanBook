754bbf4fff9f1d1d2e10b059df01defe08ed7d6f
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-04-20 19:53:08 +0200

    06: Entry class and its display in Listbox.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from tkinter import *
 
+
+class Entry:
+    def __init__(self, name, surname, telephone, city, street):
+        self.name = name
+        self.surname = surname
+        self.telephone = telephone
+        self.city = city
+        self.street = street
+
+    def getStr(self):
+        return self.name+" | "+self.surname+" | "+self.telephone+" | "+self.city+" | "+self.street
+
+
+
 root = Tk()
 root.geometry('600x400')
 root.title("")
+
 
 a = Label(root, text = "Text.")
 a.pack()
@@ -10,10 +25,11 @@ a.pack()
 scroll_bar = Scrollbar(root)
 scroll_bar.pack(side=RIGHT, fill = Y)
 
-mylist = Listbox(root, yscrollcommand=scroll_bar.set, width = 580, bg="grey")
+mylist = Listbox(root, yscrollcommand=scroll_bar.set, width = 580, bg="lightgrey")
 
-for line in range (1, 4):
-    mylist.insert(END, str(line))
+one = Entry("A", "B", "1", "C", "D")
+mylist.insert(END, one.getStr()) 
+
 
 mylist.pack(side = LEFT, fill = BOTH)
 
